e294b2e89862a31b91e8c3654e58c865dc9c0985
Youzi <94599731+Youz16@users.noreply.github.com>
2023-05-23 12:22:53 +0200

    Added currency and changed amount to int in Bid.cs

--- Auctions/Models/Bid.cs
+++ Auctions/Models/Bid.cs
@@ -11,7 +11,8 @@ public class Bid
     public int BidId { get; set; }
     public int AuctionId { get; set; }
     public string? UserId { get; set; }
-    public Int64 Amount { get; set; }
+    public string? Currency { get; set; }
+    public int Amount { get; set; }
     public DateTime BidDate { get; set; }
 
 
