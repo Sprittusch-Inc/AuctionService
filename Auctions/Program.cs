136c458b5384e20a2e367e6357140f0fd9881646
Youzi <Yousefche16@gmail.com>
2023-05-25 12:34:08 +0200

    Minor changes in program.cs

--- Auctions/Program.cs
+++ Auctions/Program.cs
@@ -17,27 +17,12 @@ logger.Debug("init main");
 var builder = WebApplication.CreateBuilder(args);
 
 Vault vault = new();
-
 string myIssuer = vault.GetSecret("authentication", "issuer").Result;
 string mySecret = vault.GetSecret("authentication", "secret").Result;
-builder.Services
-.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-.AddJwtBearer(options =>
-{
-    options.TokenValidationParameters = new TokenValidationParameters()
-    {
-        ValidateIssuer = true,
-        ValidateLifetime = true,
-        ValidateIssuerSigningKey = true,
-        ValidIssuer = myIssuer,
-        IssuerSigningKey =
-    new SymmetricSecurityKey(Encoding.UTF8.GetBytes(mySecret))
-    };
-});
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -45,19 +30,17 @@ builder.Logging.ClearProviders();
 builder.Host.UseNLog();
 
 builder.Services
-    .AddAuthentication()
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(options =>
     {
         options.TokenValidationParameters = new TokenValidationParameters()
         {
             ValidateIssuer = true,
-            ValidateAudience = true,
             ValidateLifetime = true,
             ValidateIssuerSigningKey = true,
             ValidIssuer = myIssuer,
             IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(mySecret))
         };
-
     });
 
 var app = builder.Build();
